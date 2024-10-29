56b38e3205748f707c91931e79923f0641daec42
eziquexx <waftyann@gmail.com>
2024-10-29 20:42:50 +0900

    Security Config 설정

---
+++ src/main/java/com/jelee/board/config/SecurityConfig.java
@@ -0,0 +1,44 @@
+package com.jelee.board.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		
+		log.info("security config...");
+		
+		// http 인증, 인가 설정
+		http
+			.formLogin(formLogin -> formLogin
+					.loginPage("/user/login")
+					.defaultSuccessUrl("/")
+					.permitAll()
+					)
+			.authorizeHttpRequests(authorize -> authorize
+					.requestMatchers("/", "/home", "/css/**", "/js/**", "/api/**", "/board/**")
+					.permitAll()
+					.anyRequest()
+					.authenticated()
+					);
+		
+		return http.build();
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
