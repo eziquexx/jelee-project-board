624fe1fbdab9d5d8e6d73f082cdd66e9a9f76d7b
eziquexx <waftyann@gmail.com>
2024-10-30 00:22:46 +0900

    사용자 인증, 인가 서비스 추가

---
+++ src/main/java/com/jelee/board/service/CustomUserDetailService.java
@@ -0,0 +1,45 @@
+package com.jelee.board.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.jelee.board.mapper.UserMapper;
+import com.jelee.board.model.User;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+	
+	private final UserMapper userMapper;
+	
+	@Override
+	public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+		User user = userMapper.findByUserId(userId);
+		
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found with userId: " + userId);	
+		}
+		
+		List<GrantedAuthority> authorities = user.getRoles().stream()
+				.map(role -> new SimpleGrantedAuthority(role.getName()))
+				.collect(Collectors.toList());
+		
+		return new org.springframework.security.core.userdetails.User(
+				user.getUserId(),
+				user.getUserPw(),
+				user.getEnabled(),
+				true, true, true,
+				authorities
+				);
+	}
+	
+}
